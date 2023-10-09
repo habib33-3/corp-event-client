aa7af7553cefaa0ebdcfa106a4fbb2ca27d9422e
Habibur Rahman <habib.swi16@gmail.com>
2023-10-09 23:03:39 +0600

    implemented aos animation

--- src/components/EventCards/EventCards.jsx
+++ src/components/EventCards/EventCards.jsx
@@ -1,13 +1,25 @@
 import EventCard from "../EventCard/EventCard";
 import PropTypes from "prop-types";
+import { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const EventCards = ({ events }) => {
+  useEffect(() => {
+    AOS.init();
+  }, []);
+
   return (
     <div className="mb-10">
-      <h2 className="text-5xl font-bold text-center my-10 text-blue-900">Our Services</h2>
+      <h2 className="text-5xl font-bold text-center my-10 text-blue-900">
+        Our Services
+      </h2>
       <div className="space-y-10">
         {events.map((event) => (
-          <div key={event.id}>
+          <div
+            data-aos="fade-down"
+            key={event.id}
+          >
             <EventCard event={event} />
           </div>
         ))}
